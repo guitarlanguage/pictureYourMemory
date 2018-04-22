0302b78d8bb01190419b1aaf42ac0c3ed5363781
Tom LaChance <guitarlanguageseries@gmail.com>
2018-04-22 17:27:01 -0500

    deleted friends.json, inserted hockeyPlayers.json.

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./friends.json";
+import hockeyPlayers from "./hockeyPlayers.json";
 import "./App.css";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
+  // Setting this.state.hockeyPlayers to the hockeyPlayers json array
   state = {
-    friends
+    hockeyPlayers
   };
 
   removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+    // Filter this.state.hockeyPlayers for hockeyPlayers with an id not equal to the id being removed
+    const hockeyPlayers = this.state.hockeyPlayers.filter(friend => friend.id !== id);
+    // Set this.state.hockeyPlayers equal to the new hockeyPlayers array
+    this.setState({ hockeyPlayers });
   };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
+  // Map over this.state.hockeyPlayers and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
         <Title>Picture Your Memory</Title>
-        {this.state.friends.map(friend => (
+        {this.state.hockeyPlayers.map(friend => (
           <FriendCard
             removeFriend={this.removeFriend}
             id={friend.id}
