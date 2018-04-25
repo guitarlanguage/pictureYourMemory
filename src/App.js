0eb8018fa6791a67f6003b6b6f90c2272f7d7b40
Tom LaChance <guitarlanguageseries@gmail.com>
2018-04-24 21:27:48 -0500

    threw in logic for app.js

--- src/App.js
+++ src/App.js
@@ -8,16 +8,57 @@ import "./App.css";
 class App extends Component {
   // Setting this.state.hockeyPlayers to the hockeyPlayers json array
   state = {
-    hockeyPlayers
+    hockeyPlayers,
+    goal: 0,
+    highScore: 0,
+    feedback: "Hit a player to begin, but don't hit them twice.",
+    hit: []
   };
 
-  removeFriend = id => {
-    // Filter this.state.hockeyPlayers for hockeyPlayers with an id not equal to the id being removed
-    const hockeyPlayers = this.state.hockeyPlayers.filter(friend => friend.id !== id);
-    // Set this.state.hockeyPlayers equal to the new hockeyPlayers array
-    this.setState({ hockeyPlayers });
+  hitPlayer = id => {
+    if(this.state.hit.indexOf(id) === -1) {
+      this.state.hit.push(id);
+      this.scoreKeeper();
+      this.shuffleCards();
+    } else if (this.state.goal === 12) {
+        this.scoreKeeper();
+        this.resetHandler();
+    } else {
+      this.setState({feedback: "You hit the same player twice.  Hope you've got iron fists."});
+      this.resetHandler();
+    }
   };
 
+  scoreKeeper = () => {
+    const newScore = this.state.goal + 1;
+    this.setState({goal: newScore});
+    if (this.state.score === 12) {
+      this.setState({feedback: "You've moved on to the playoffs"});
+    } else {
+      this.setState({feedback: "No memory issues yet. "});
+    } 
+    if (newScore >= this.state.highScore) {
+      this.setState({highScore: newScore});
+    }
+  };
+
+  lineChange = () => {
+    let arr = this.state.hockeyPlayers;
+    arr.forEach(function(elem, item) {
+      let j = Math.floor(Math.random() * (item + 1));
+      [arr[item], arr[j] = arr[j], arr[item]];
+    });
+    return arr;
+  };
+
+  resetLineUp = () => {
+    this.setState({
+    goal: 0,
+    highScore: this.state.highScore,
+    hit: []
+    });
+    this.lineChange();
+  }
   // Map over this.state.hockeyPlayers and render a FriendCard component for each friend object
   render() {
     return (
@@ -25,7 +66,7 @@ class App extends Component {
         <Title>Picture Your Memory</Title>
         {this.state.hockeyPlayers.map(friend => (
           <FriendCard
-            removeFriend={this.removeFriend}
+            hitPlayer={this.hitPlayer}
             id={friend.id}
             key={friend.id}
             name={friend.name}
