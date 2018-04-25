12dff9d86fa447c7357897b1ba9feb476c8a0126
Tom LaChance <guitarlanguageseries@gmail.com>
2018-04-24 23:27:25 -0500

    added back the image information. Make it more fun

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -5,6 +5,20 @@ const FriendCard = props => (
   <div className="card">
     <div className="img-container">
       <img alt={props.name} src={props.image} value={props.id} onClick={()=> props.hitPlayer(props.id)}/>
+      <h5>Name: {props.name}</h5>
+    </div>
+    <div className="content">
+      <ul>
+        <li>
+          <strong>Name:</strong> {props.name}
+        </li>
+        <li>
+          <strong>Occupation:</strong> {props.occupation}
+        </li>
+        <li>
+          <strong>Location:</strong> {props.location}
+        </li>
+      </ul>
     </div>
   </div> 
 );
