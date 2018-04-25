42c3deec66ee77338753c10c3010be4e19f6a347
Tom LaChance <guitarlanguageseries@gmail.com>
2018-04-24 22:10:10 -0500

    app seems to be working fine

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -4,25 +4,9 @@ import "./FriendCard.css";
 const FriendCard = props => (
   <div className="card">
     <div className="img-container">
-      <img alt={props.name} src={props.image} />
+      <img alt={props.name} src={props.image} value={props.id} onClick={()=> props.hitPlayer(props.id)}/>
     </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Location:</strong> {props.location}
-        </li>
-      </ul>
-    </div>
-    <span onClick={() => props.hitPlayer(props.id)}>
-      𝘅
-    </span>
-  </div>
+  </div> 
 );
 
 export default FriendCard;
